14931f050dc03a091e0deabf970142007bc57f19
b-richard <benjamin.richard-exterieur@fiducial.net>
2018-03-20 12:33:10 +0100

    transpile seems ok

---
+++ index.js
@@ -0,0 +1,9 @@
+require ('babel-polyfill')
+// /require ('core-js/modules/es6.array.for-each.js')
+
+console.log('start')
+
+const list = [1, 2, 3, 4]
+list.forEach(val => console.log(val))
+
+console.log('end')
